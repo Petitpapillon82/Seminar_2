37d19a62cb63c4bba4da77210fc76720aabb1022
Irina <petitpapillon82@gmail.com>
2022-08-23 03:09:07 +0400

    Add T1.

---
+++ T1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10,99] и показывает наибольшую цифру числа
+
+
+
+Console.Clear();
+
+int num = new Random().Next(10, 100); //74
+
+int a1 = num / 10;
+int a2 = num % 10;
+
+Console.WriteLine(num);
+
+int max = a1;
+if (a2 > max)
+{
+    max = a2;
+}
+Console.WriteLine(max);
