0d6ab5fa9e550b67f6eda16b9132c160dfa21f31
Irina <petitpapillon82@gmail.com>
2022-08-23 03:18:07 +0400

    Add T5.

---
+++ T5/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+if (numberA == numberB * numberB)
+{
+    Console.WriteLine($"Число {numberA} - квадрат числа {numberB}");
+}
+else if (numberB == numberA * numberA)
+{
+   Console.WriteLine($"Число {numberB} - квадрат числа {numberA}"); 
+}
+else
+{
+   Console.WriteLine ("Квадрата нет");
+}
