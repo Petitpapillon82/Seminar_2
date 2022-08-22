b7a69bbaebe9f0f28de6ec1108352096387a3844
Irina <petitpapillon82@gmail.com>
2022-08-23 03:13:26 +0400

    Add T2.

---
+++ T2/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+
+int num = new Random().Next(100, 1000); 
+Console.WriteLine(num);
+
+int a1 = num / 100;
+int a2 = num % 10;
+
+Console.Write(a1);
+Console.WriteLine(a2);
+
+
+//Console.Clear();
+//int number = new Random().Next(100, 1000); 
+//Console.WriteLine(number);
+//int digit100 = number / 100;
+//int digit1 = number % 10;
+//Console.WriteLine($"digit100 is: {digit100}");
+//Console.WriteLine($"digit1 is: {digit1}");
+//Console.WriteLine("the result is: " + (digit100 * 10 + digit1));
+
+
+//Console.Clear();
+//int num = new Random().Next(100, 1000); 
+//Console.WriteLine(num);
+//int a1 = (num / 100) * 10;
+//int a2 = num % 10;
+//int max = a1 + a2;
+//Console.WriteLine(max);
+
+
+//int n = new Random().Next(100, 1000); 
+// //int a = n / 100;
+// //int b = n % 10;
+//Console.WriteLine(n);
+//Console.WriteLine((n / 100)*10 + n % 10);
