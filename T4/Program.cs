d3d2ffc7cab91409a49c0f2f056d5d60103d6bf2
Irina <petitpapillon82@gmail.com>
2022-08-23 03:16:34 +0400

    Add T4.

---
+++ T4/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход число и проверяет, 
+//кратно ли оно одновременно 7 и 23.
+//14 -> нет
+// 46 -> нет 
+//161 -> да
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(n % 7 == 0 && n % 23 == 0 ? "Да" : "Нет");
+
+//Console.Clear();
+// //int number = new Random().Next(100, 1000); 
+// //Console.WriteLine(number);
+//int number = 162;
+// //Console.WriteLine(number % 23 == 0 && number % 7 ==0 ? $"Number {number} is aliquote to 7 and 23" : $"Number {number} is NOT aliquote to 7 and 23");
+//if (number % 23 == 0 && number % 7 == 0)
+//{
+    //Console.WriteLine($"Number {number} is aliquote to 7 and 23");
+//}
+//else
+//{
+    //Console.WriteLine($"Number {number} is NOT aliquote to 7 and 23");
+//}
